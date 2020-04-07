363cc1138360db62dac0a98384c3b3ed4c3be7d1
Tobey Luciando <42970574+symeRobinson@users.noreply.github.com>
2020-04-07 11:51:16 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+games { 'gta5' }
+client_scripts {
+  'configuration.lua',
+  'client.lua'
+}
+server_scripts {
+  'configuration.lua',
+  'server.lua'
+}
+ui_page('client/html/index.html')
+
+files { 
+    'base/html/index.html',
+    'base/html/sounds/tone.ogg'
+}
